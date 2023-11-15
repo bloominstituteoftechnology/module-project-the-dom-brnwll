a05967f7bfc89a5995fe387f62b12c0a25fa47d1
Brian Howell <brianhowell@pm.me>
2023-11-14 20:33:21 -0500

    Tasks 3 and 4 - Tests pass

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,7 @@ function moduleProject1() {
 
   // 👇 WORK WORK BELOW THIS LINE 👇
   const widgetSection = document.querySelector('section')
+  const random = data => data[Math.floor(Math.random() * data.length)]
 
   // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
   const widgetDivs = widgetSection.querySelectorAll('div')
@@ -12,20 +13,36 @@ function moduleProject1() {
 
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
-  const quoteObj = quotes[Math.floor(Math.random() * quotes.length)];
+  const quoteData = random(quotes);
   const quoteContainer = widgetSection.querySelector('.quoteoftheday')
   const quoteDiv = document.createElement('div')
-  quoteDiv.textContent = quoteObj.quote
+  quoteDiv.textContent = quoteData.quote
   quoteContainer.appendChild(quoteDiv)
   const authorDiv = document.createElement('div')
-  authorDiv.textContent = `${quoteObj.author} in ${quoteObj.date ? quoteObj.date : 'an unknown date'}`
+  authorDiv.textContent = `${quoteData.author} in ${quoteData.date ? quoteData.date : 'an unknown date'}`
   quoteContainer.appendChild(authorDiv)
 
   // 👉 TASK 3 - Build a "Corporate Speak" widget
-  
+  const randomCorporateSpeak = () => `We need to ${random(verbs)} our ${random(nouns)} ${random(adverbs)} in order to ${random(verbs)} our ${random(nouns)} ${random(adverbs)}.`
+  const corpSpeak = document.createElement('p')
+  corpSpeak.textContent = randomCorporateSpeak()
+  widgetSection.querySelector('.corporatespeak').appendChild(corpSpeak)
 
   // 👉 TASK 4 - Build a "Countdown" widget
-  //  ✨ add your code here
+  const countdownEl = document.createElement('p')
+  widgetSection.querySelector('.countdown').appendChild(countdownEl)
+
+  let count = 5
+  const countdown = count => {
+    countdownEl.textContent = count ? `T-minus ${count}...` : 'Liftoff! 🚀'
+    count--
+    if (count >= 0) {
+      setTimeout(() => {
+        countdown(count)
+      }, 1000)
+    }
+  }
+  countdown(count)
 
   // 👉 TASK 5 - Build a "Friends" widget
   //  ✨ add your code here
